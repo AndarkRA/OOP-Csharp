ed4098628a2421dcb5e2b0e3f675f2d6a6c46607
AndarkRA <klushin1973@yandex.ru>
2022-07-07 21:42:56 +0300

    HW02Task01

---
+++ Homework02/Homeworks_OOP/Homeworks_OOP/BankAccount.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks_OOP
+{
+    enum AccountType
+    {
+        Credit,
+        Debit,
+        Current
+    }
+
+    /// <summary>
+    /// Создать класс счет в банке с закрытыми полями: номер счета, баланс, тип
+    /// банковского счета(использовать перечислимый тип). Предусмотреть методы для
+    /// доступа к данным – заполнения и чтения.Создать объект класса, заполнить его
+    /// поля и вывести информацию об объекте класса на печать
+    /// </summary>
+    internal class BankAccount
+    {
+        public override string ToString() => $"[{BankNumber}] ({Balance}) {Type}";
+
+        private int _bankNumber;
+        private decimal _balance;
+        private AccountType _type;
+
+
+        public int BankNumber
+        {
+           get => _bankNumber;
+           set => _bankNumber = value;
+        }
+
+        public decimal Balance
+        {
+            get => _balance;
+            set => _balance = value;
+        }
+
+        public AccountType Type
+        {
+            get => _type;
+            set => _type = value;
+        }
+
+    }
+}
--- Homework02/Homeworks_OOP/Homeworks_OOP/Program.cs
+++ Homework02/Homeworks_OOP/Homeworks_OOP/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Homeworks_OOP;
+
+BankAccount bankAccount = new BankAccount();
+bankAccount.BankNumber = 1412344;
+bankAccount.Balance = 10000;
+bankAccount.Type = AccountType.Debit;
+
+
+Console.WriteLine($"Номер счета: {bankAccount.BankNumber}\n" +
+                  $"Баланс: {bankAccount.Balance}\n" +
+                  $"Тип банковского счета: {bankAccount.Type}");
