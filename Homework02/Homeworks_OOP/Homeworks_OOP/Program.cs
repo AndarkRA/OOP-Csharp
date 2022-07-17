e43ea95e4f388e892c6ab36ef54d3c14360c2216
AndarkRA <klushin1973@yandex.ru>
2022-07-17 20:57:45 +0300

    HW05Task01

--- Homework02/Homeworks_OOP/Homeworks_OOP/Program.cs
+++ Homework02/Homeworks_OOP/Homeworks_OOP/Program.cs
@@ -1,31 +1,26 @@
 ﻿using Homeworks_OOP;
 
 /// <summary>
-///  Реализовать класс для описания здания (уникальный номер здания, высота, этажность, количество квартир, подъездов). 
-///  Поля сделать закрытыми, предусмотреть методы для заполнения полей и получения значений полей для печати. 
-///  Добавить методы вычисления высоты этажа, количества квартир в подъезде, количества квартир на этаже и т.д. 
-///  Предусмотреть возможность, чтобы уникальный номер здания генерировался программно. Для этого в классе предусмотреть статическое поле, 
-///  которое бы хранило последний использованный номер здания, и предусмотреть метод, который увеличивал бы значение этого поля.
+///  Создать класс рациональных чисел. В классе два поля – числитель и знаменатель. 
+///  Предусмотреть конструктор. Определить операторы ==, != (метод Equals()), <, >, <=, >=, +, –, ++, --. 
+///  Переопределить метод ToString() для вывода дроби. 
+///  Определить операторы преобразования типов между типом дробь,float, int. Определить операторы *, /, %.
 /// </summary>
 
-Building building = new Building();
-building.ChangeApartmentCount(10);
-building.ChangeEntranceCount(12);
-building.ChangeFloorCount(3);
-building.ChangeHeight(125);
+RatioNumber number1 = new RatioNumber(5, 10);
+RatioNumber number2 = new RatioNumber(2, 10);
 
-Console.WriteLine(building);
-Console.WriteLine();
-Console.WriteLine("Количество квартир на этаже: " + building.GetApartmentCountInFloor());
-Console.WriteLine("Высота этажа: " + building.GetFloorHeight() + "м\n");
+Console.WriteLine(number1 == number2);
+Console.WriteLine(number1 != number2);
+Console.WriteLine(number1 < number2);
+Console.WriteLine(number1 <= number2);
+Console.WriteLine(number1 > number2);
+Console.WriteLine(number1 >= number2);
+Console.WriteLine($"{ number1}, {number2}");
 
-// убедимся, что id увеличивается
+//Console.Clear();
+//float number3 = number1 * number2;
+Console.WriteLine((number1 / number2).ToString());
+Console.WriteLine((number1 % number2).ToString());
 
-for (int i = 0; i < 100; i++)
-{
-    Building building2 = new Building();
-    Console.WriteLine(building2.ID);
-}
-
-Console.ReadKey();
 Console.ReadKey();
