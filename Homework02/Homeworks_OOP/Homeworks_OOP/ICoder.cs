7bda5be5ae51748dd4d62536d9d473a037b4afc1
AndarkRA <klushin1973@yandex.ru>
2022-07-31 18:52:38 +0300

    HW07Task01

---
+++ Homework02/Homeworks_OOP/Homeworks_OOP/ICoder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homeworks_OOP
+{
+    public interface ICoder
+    {
+        string Encode(string baseString);
+
+        string Decode(string baseString);
+    }
+}
